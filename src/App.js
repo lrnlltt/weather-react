2f83b95a734146c932260811437cc30d78efff52
lrnlltt <73768428+lrnlltt@users.noreply.github.com>
2021-02-19 11:41:42 +1100

    Display weather with axios integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
-function App() {
+function App(props) {
+  const [temperature, setTemperature] = useState("");
+
+  function findTemp(response) {
+    setTemperature(response.data.main.temp);
+  }
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=560ccf0a9b6f4d30ed340bdb4dfaf585&units=metric`
+  axios.get(apiUrl).then(findTemp);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>The temperature is currently {Math.round(temperature)}° in {props.city}</h2>
     </div>
   );
 }
